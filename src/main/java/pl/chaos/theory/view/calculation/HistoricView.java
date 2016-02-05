7dfe535e20f73c0d7e3d1553d3d8637818a15f8b
slawek <kowalskislawomirkarol@gmail.com>
2016-02-05 12:18:43 +0100

    historic view - dialog with parameters for selected row

---
+++ src/main/java/pl/chaos/theory/view/calculation/HistoricView.java
@@ -0,0 +1,25 @@
+package pl.chaos.theory.view.calculation;
+
+import org.springframework.stereotype.Component;
+import pl.chaos.theory.dto.model.ParameterDto;
+import pl.chaos.theory.util.Util;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.view.ViewScoped;
+import java.util.HashSet;
+import java.util.Set;
+
+@ViewScoped
+@ManagedBean(name = "historicV")
+@Component("historic")
+public class HistoricView {
+	private final Set<ParameterDto> parameters = new HashSet<ParameterDto>();
+
+	public Set<ParameterDto> getParameters() {
+		return parameters;
+	}
+
+	public void setParameters(Set<ParameterDto> parameters) {
+		Util.copyCollection(this.parameters, parameters);
+	}
+}
